748acfc12b9558c31360d97e4e19d0aefb59760c
Emir Gulsen <111822941+emrglsn@users.noreply.github.com>
2023-07-02 03:02:38 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use("/",productRouter)
 app.use("/companies",companyRouter)
 app.use("/auth",userRouter)
 
-mongoose.connect("mongodb+srv://emirglsn191:emirglsn191@cluster0.rabr2dw.mongodb.net/ecommerce?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://.................@cluster0.rabr2dw.mongodb.net/ecommerce?retryWrites=true&w=majority")
 
 app.listen(3001, () => {
     console.log("Server connected succesfully to 3001 port");
